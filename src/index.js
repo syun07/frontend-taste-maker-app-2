5172044d749f6f332dc76c4d3380e16ff8893739
Sarah Yun <sarahyun7@gmail.com>
2019-08-16 17:07:52 -0400

    fix weird bg image format

--- src/index.js
+++ src/index.js
@@ -3,19 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-
 import * as serviceWorker from './serviceWorker';
 import allReducers from './reducers/allReducers';
-
+import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 
 let store = createStore(allReducers)
 
 ReactDOM.render(
-	<Provider store={store}>
-	<App />
-	</Provider>,
+	<BrowserRouter>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</BrowserRouter>,
 	document.getElementById('root')
 );
 
